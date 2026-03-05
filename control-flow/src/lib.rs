75e3fd34e4a7f6364ec861dbe6f609adf2195716
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2026-03-05 11:17:02 +0000

    last challnege done slices

--- control-flow/src/lib.rs
+++ control-flow/src/lib.rs
@@ -2,6 +2,15 @@ pub fn check_number_sign(number: i32) -> String {
     // Return `"positive"` if the number is positive.
     // Return `"negative"` if the number is negative.
     // Return `"zero"` if the number is zero.
+    if number > 0 {
+        "positive"
+    } else if number < 0 {
+        "negative"
+    } else {
+        "zero"
+    }
+    .to_string()
+}
 
     // Step 1:
     // Check if the number is positive.
@@ -11,4 +20,4 @@ pub fn check_number_sign(number: i32) -> String {
 
     // Step 3:
     // Handle the case where it's neither positive nor negative.
-}
+
